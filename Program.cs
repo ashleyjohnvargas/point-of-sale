953500499e206c53a617d95b2b08ea90f2e4a254
CherylPlj <cjeannepalaje@gmail.com>
2025-01-08 22:29:17 +0800

    For Deployment Frontend only

--- Program.cs
+++ Program.cs
@@ -54,13 +54,13 @@ app.UseSession(); // This should come before UseAuthorization
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"); 
-
 //app.MapControllerRoute(
 //    name: "default",
-//    pattern: "{controller=Login}/{action=LoginPage}/{id?}");
+//    pattern: "{controller=Home}/{action=Index}/{id?}"); 
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Login}/{action=LoginPage}/{id?}");
 
 
 
