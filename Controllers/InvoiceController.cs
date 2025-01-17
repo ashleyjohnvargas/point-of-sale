56ce5b11d932c274097e446fa9ac475a37b0cce3
ashleyjohnvargas <vashleyjohn@gmail.com>
2025-01-17 17:20:44 +0800

    download_pdf_of_invoice_done

---
+++ Controllers/InvoiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace POS1.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
